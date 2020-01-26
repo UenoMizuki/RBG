9b4554e103ce841f42d906de55c31baed5695806
Sigma1023 <1m0turbo2max3@gmail.com>
2020-01-26 14:57:31 +0900

    setting Phaser3 configs

--- TS/webpack.config.js
+++ TS/webpack.config.js
@@ -5,37 +5,38 @@ module.exports =env => {
     // 下記はオブジェクトとして指定した例 
     return {
         entry : './src/'+env+'.ts',
-    output : {
-        filename    : env+'.js',
-        path        : path.resolve(__dirname, '../src/main/resources/static/js')
-    },
-    /*
-    entry: {
-        './src/'+env.name+'.ts'
-    },  
-    output: {
-        // モジュールバンドルを行った結果を出力する場所やファイル名の指定
-        // "__dirname"はこのファイルが存在するディレクトリを表すnode.jsで定義済みの定数
-        path: path.join(__dirname,'dist'),
-        filename: '[name].js'  // [name]はentryで記述した名前(この例ではbundle）が入る
-    },*/
-    // モジュールとして扱いたいファイルの拡張子を指定する
-    // 例えば「import Foo from './foo'」という記述に対して"foo.ts"という名前のファイルをモジュールとして探す
-    // デフォルトは['.js', '.json']
-    resolve: {
-        extensions:['.ts','.js']
-    },
-    devServer: {
-        // webpack-dev-serverの公開フォルダ
-        contentBase: path.join(__dirname,'dist')
-    },
-    // モジュールに適用するルールの設定（ここではローダーの設定を行う事が多い）
-    module: {
-        rules: [
-            {
-                // 拡張子が.tsで終わるファイルに対して、TypeScriptコンパイラを適用する
-                test:/\.ts$/,loader:'ts-loader'
-            }
-        ]
-    }}
+        output : {
+            filename    : env+'.js',
+            path        : path.resolve(__dirname, '../src/main/resources/static/js')
+        },
+        /*
+        entry: {
+            './src/'+env.name+'.ts'
+        },  
+        output: {
+            // モジュールバンドルを行った結果を出力する場所やファイル名の指定
+            // "__dirname"はこのファイルが存在するディレクトリを表すnode.jsで定義済みの定数
+            path: path.join(__dirname,'dist'),
+            filename: '[name].js'  // [name]はentryで記述した名前(この例ではbundle）が入る
+        },*/
+        // モジュールとして扱いたいファイルの拡張子を指定する
+        // 例えば「import Foo from './foo'」という記述に対して"foo.ts"という名前のファイルをモジュールとして探す
+        // デフォルトは['.js', '.json']
+        resolve: {
+            extensions:['.ts','.js']
+        },
+        devServer: {
+            // webpack-dev-serverの公開フォルダ
+            contentBase: path.join(__dirname,'dist')
+        },
+        // モジュールに適用するルールの設定（ここではローダーの設定を行う事が多い）
+        module: {
+            rules: [
+                {
+                    // 拡張子が.tsで終わるファイルに対して、TypeScriptコンパイラを適用する
+                    test:/\.ts$/,loader:'ts-loader'
+                }
+            ]
+        }
+    }
 }
